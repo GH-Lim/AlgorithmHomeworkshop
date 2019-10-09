79f78b8d81fa0c6eb138a2bef52c30f75a014adb
임건혁 <gunhyuck11@naver.com>
2019-10-09 20:34:12 +0900

    upload

---
+++ homework/2기서울2반10월10일임건혁/1231_중위순회.py
@@ -0,0 +1,27 @@
+def in_order(node):
+    l, r = L[node]
+    if l:
+        in_order(l)
+        print(nodes[node], end='')
+        if r:
+            in_order(r)
+    else:
+        print(nodes[node], end='')
+
+
+for tc in range(1, 11):
+    N = int(input())
+    nodes = ['0'] * (N + 1)
+    L = [[0, 0] for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        info = input().split()
+        l, r = 0, 0
+        if len(info) > 2:
+            l = int(info[2])
+            if len(info) == 4:
+                r = int(info[3])
+        nodes[i] = info[1]
+        L[i][0], L[i][1] = l, r
+    print('#%d ' % tc, end='')
+    in_order(1)
+    print()
